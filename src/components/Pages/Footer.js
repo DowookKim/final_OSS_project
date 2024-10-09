3b8e1e7adb2b80e21cc7c5945c69723c70334d19
HyeokjuCHu <22100062@handong.ac.kr>
2024-10-09 22:13:32 +0900

    UI&form

--- src/components/Pages/Footer.js
+++ src/components/Pages/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2024 Your Bookstore Name. All rights reserved.</p>
+        <p>&copy; 2024 Kim&Kwon Book. All rights reserved.</p>
         <div className="footer-links">
           <a href="#">About Us</a>
           <a href="#">Contact</a>
