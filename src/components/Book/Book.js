f242916b5cb44155b43c4be728bfe21f8695feda
HyeokjuCHu <22100062@handong.ac.kr>
2024-10-19 13:52:04 +0900

    Cart

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,18 +2,19 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Book.css";
+
 const EditBook = () => {
-  const [book, setBook] = useState([]);
+  const [book, setBook] = useState({});
   const { id } = useParams();
   const getBookApi = "https://66ff38182b9aac9c997e8ef9.mockapi.io/api/oss/books";
 
   useEffect(() => {
     getBook();
-  });
+  }, [id]);
 
   const getBook = () => {
     axios
-      .get(getBookApi.concat("/") + id)
+      .get(`${getBookApi}/${id}`)
       .then((item) => {
         setBook(item.data);
       })
@@ -25,31 +26,39 @@ const EditBook = () => {
   return (
     <div className="user mt-5">
       <table className="table table-bordered">
-
-    <tbody>
-      <tr>
-        <td>Book Name</td>
-        <td>{book.name}</td>
-      </tr>
-      <tr>
-        <td>Publishing name</td>
-        <td>{book.puname}</td>
-      </tr>
-      <tr>
-        <td>Writer</td>
-        <td>{book.byname}</td>
-      </tr>
-      <tr>
-        <td>price</td>
-        <td>{book.price}</td>
-      </tr>
-      <tr>
-        <td>Rental Status</td>
-        <td>{book.message}</td>
-      </tr>
-    </tbody>
-  </table>
+        <tbody>
+          <tr>
+            <td>Book Name</td>
+            <td>{book.name}</td>
+          </tr>
+          <tr>
+            <td>Publishing Name</td>
+            <td>{book.puname}</td>
+          </tr>
+          <tr>
+            <td>Writer</td>
+            <td>{book.byname}</td>
+          </tr>
+          <tr>
+            <td>Price</td>
+            <td>{book.price}</td>
+          </tr>
+          <tr>
+            <td>Rental Status</td>
+            <td>{book.message ? "Available" : "Not Available"}</td>
+          </tr>
+          <tr>
+            <td>ISBN</td>
+            <td>{book.isbn}</td>
+          </tr>
+          <tr>
+            <td>Publication Date</td>
+            <td>{book.pudate ? book.pudate.slice(0, 10) : ""}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
+
 export default EditBook;
